18082d67c3693eb07649404d9e25279b0494e371
IgorRRamos <ramosigorprofissional@gmail.com>
2022-11-04 14:39:03 -0300

    Exercicio jogador

--- java/ExerciciosAleatorios/exercicio jogador/src/Main.java
+++ java/ExerciciosAleatorios/exercicio jogador/src/Main.java
@@ -1,9 +1,29 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import br.com.igorramos.Jogador;
+import br.com.igorramos.ListaDeJogadores;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Jogador> jogador = new ArrayList<Jogador>();
+        ListaDeJogadores listTeam = new ListaDeJogadores();
+        Scanner teclado = new Scanner(System.in);
+
+        int choice;
+
+        do{
+            System.out.println("1 - Cadastrar jogador");
+            System.out.println("2 - Tirar jogador do time");
+            choice = teclado.nextInt();
+
+            switch (choice){
+
+                case 1 -> listTeam.registrar();
+
+            }
+
+        }while(choice != 0);
+
 
 
 
