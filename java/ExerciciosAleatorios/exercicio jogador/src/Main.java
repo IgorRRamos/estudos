93ccc185c44b5bbb0579b736331c0d6b1685f842
IgorRRamos <ramosigorprofissional@gmail.com>
2022-11-05 12:07:32 -0300

    Exercicio jogador

--- java/ExerciciosAleatorios/exercicio jogador/src/Main.java
+++ java/ExerciciosAleatorios/exercicio jogador/src/Main.java
@@ -1,34 +1,39 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
 import br.com.igorramos.Jogador;
 import br.com.igorramos.ListaDeJogadores;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Jogador> jogador = new ArrayList<Jogador>();
-        ListaDeJogadores listTeam = new ListaDeJogadores();
+        ArrayList<Jogador> jogador = new ArrayList<>();
+        ListaDeJogadores listTeam = new ListaDeJogadores(jogador);
         Scanner teclado = new Scanner(System.in);
 
         int choice;
 
-        do{
+        do {
             System.out.println("1 - Cadastrar jogador");
             System.out.println("2 - Tirar jogador do time");
             System.out.println();
             System.out.print("Selecione uma opção: ");
             choice = teclado.nextInt();
 
-            switch (choice){
+            switch (choice) {
 
-                case 1 -> listTeam.registrar();
-
-                case 2 -> listTeam.removePlayer();
-
-                default -> {if(choice != 0) System.out.println("Opção invalida!");}
+                case 1:
+                    listTeam.registrar();
+                    break;
+                case 2:
+                    listTeam.removePlayer();
+                    break;
+                default:
+                    if (choice != 0) System.out.println("Opção invalida!");
+                    break;
 
             }
 
-        }while(choice != 0);
+        } while (choice != 0);
 
 
     }
--- java/ExerciciosAleatorios/exercicio jogador/src/br/com/igorramos/ListaDeJogadores.java
+++ java/ExerciciosAleatorios/exercicio jogador/src/br/com/igorramos/ListaDeJogadores.java
@@ -1,4 +1,5 @@
 package br.com.igorramos;
+
 import br.com.igorramos.Jogador;
 
 import java.util.ArrayList;
@@ -9,7 +10,12 @@ public class ListaDeJogadores {
     public ArrayList<Jogador> jogador;
 
 
-    public void registrar(){
+    public ListaDeJogadores(ArrayList<Jogador> jogador) {
+        this.jogador = jogador;
+    }
+
+
+    public void registrar() {
         System.out.print("Nome do jogador: ");
         String nome = teclado.nextLine();
         System.out.print("Idade do jogador: ");
@@ -22,11 +28,19 @@ public class ListaDeJogadores {
         jogador.add(newPlayer);
     }
 
-    public void removePlayer(){
-        jogador.remove(0);
+    public void removePlayer() {
+        String nome;
+        System.out.print("Digite o nome do jogador: ");
+        nome = teclado.nextLine();
+
+        for (int i = 0; i <= jogador.size(); i++) {
+            if (nome.equals(jogador.get(i))) {
+                jogador.remove(jogador.get(i));
+            } else {
+                System.out.println("Jogador não Encontrado!!");
+            }
+        }
     }
-
-
 
     public ArrayList<Jogador> getJogador() {
         return jogador;
